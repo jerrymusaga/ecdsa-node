19f6c8a753cde1b261fd4aed96f2d45cb2cb7cd9
jerrymusaga <musagatechnology@gmail.com>
2023-03-14 18:27:56 +0100

    generate public key from private key

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -4,3 +4,6 @@ const {toHex } = require("ethereum-cryptography/utils");
 const privateKey = secp.utils.randomPrivateKey();
 
 console.log(`private key: ${toHex(privateKey)}`)
+
+const publicKey = secp.getPublicKey(privateKey)
+console.log(`public key: ${toHex(publicKey)}`)
